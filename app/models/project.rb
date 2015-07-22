bccb76f2041e0e5c69a29b503afce257a569d831
Conco MB <cmaderblanco@wolox.com.ar>
2015-07-22 16:36:09 -0300

    7

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,12 @@
 class Project < ActiveRecord::Base
 
-  MAX_PARTICIPANTS = 5
+  MAX_PARTICIPANTS = 7
 
   has_many :users
   has_many :project_exceptions
   validates :users, length: { maximum: MAX_PARTICIPANTS }
   validates :name, :description, presence: true
-  
+
   def can_participate?(user)
     users.length != MAX_PARTICIPANTS && user.project.nil?
   end
@@ -19,7 +19,7 @@ class Project < ActiveRecord::Base
     users.size == MAX_PARTICIPANTS
   end
 
-  def can_request_exception?(user) 
+  def can_request_exception?(user)
     !participates?(user) && is_full? && user.project.blank? && !has_requested_exception?(user)
   end
 
